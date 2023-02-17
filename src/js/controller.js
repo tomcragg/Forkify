a0ff3879b5499a9764ef4365d219d7bdaeb249f4
Tom Cragg <tom.cragg1@gmail.com>
2023-02-17 19:50:29 +0100

    Implemented searchView.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,11 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable'; //polyfill ES6 features for old browsers
 import 'regenerator-runtime/runtime'; //polyfill ES6 features for old browsers
 
 // https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
@@ -26,8 +25,25 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
