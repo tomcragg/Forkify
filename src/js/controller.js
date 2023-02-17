f7ef9fea340e7763d21f29305c1813618f522ecf
Tom Cragg <tom.cragg1@gmail.com>
2023-02-17 20:33:27 +0100

    resultsView _generateMarkup() done

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable'; //polyfill ES6 features for old browsers
 import 'regenerator-runtime/runtime'; //polyfill ES6 features for old browsers
@@ -27,6 +28,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -36,6 +39,7 @@ const controlSearchResults = async function () {
 
     // 3) Render results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
