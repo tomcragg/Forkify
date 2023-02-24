ee0ed76e57327e85a06fc194d1dbc7a32e4bf2f5
Tom Cragg <tom.cragg1@gmail.com>
2023-02-24 18:19:37 +0100

    DOM updating algorithm completed.

--- src/js/controller.js
+++ src/js/controller.js
@@ -23,6 +23,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1) loading recipe
     await model.loadRecipe(id);
 
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -7,21 +7,28 @@ class ResultsView extends View {
   _message = '';
 
   _generateMarkup() {
+    console.log(this._data);
     return this._data.map(this._generateMarkupPreview).join('');
   }
 
   _generateMarkupPreview(result) {
+    const id = window.location.hash.slice(1);
+
+    console.log(id, result.id);
+
     return `
         <li class="preview">
-            <a class="preview__link" href="#${result.id}">
-            <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
-                </figure>
-                <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>  
-                </div>
-            </a>
+          <a class="preview__link ${
+            result.id === id ? 'preview__link--active' : ''
+          }" href="#${result.id}">
+          <figure class="preview__fig">
+            <img src="${result.image}" alt="${result.title}" />
+            </figure>
+            <div class="preview__data">
+            <h4 class="preview__title">${result.title}</h4>
+            <p class="preview__publisher">${result.publisher}</p>  
+            </div>
+          </a>
         </li> 
     `;
   }
